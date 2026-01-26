663133678fd2c77ed29d2be10d9c3c6cf207ca98
wishuponsirius <nnthienkim2905@gmail.com>
2026-01-27 02:07:14 +0700

    fix import bug

--- investment-system-fe/src/pages/admin/AdminDashboard.jsx
+++ investment-system-fe/src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 import styles from "../../pages/admin/AdminDashboard.module.css";
 
 const AdminDashboard = () => {
